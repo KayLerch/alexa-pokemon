5cc210433b1efbd912a226bc7d623cc79f748d0e
nikdin <nikdin@172.27.116.23>
2017-05-04 20:10:15 +0100

    Test

--- src/main/java/io/klerch/alexa/pokemon/api/CityMapper.java
+++ src/main/java/io/klerch/alexa/pokemon/api/CityMapper.java
@@ -22,48 +22,57 @@ public class CityMapper {
 	private static final double originLongitude = -0.136138;
 
 	private static final double originLatitude = 51.510453;
-	
 
-	public static String getRouteToPokemon(final double longitude, final double latitude) throws ApiException, InterruptedException, IOException {
-		GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyBiUikU1C1pYgqE5DWbFYqZGmn_PGktu2k");
-		DateTime now = new DateTime();
-		DirectionsResult result = DirectionsApi.newRequest(context).mode(TravelMode.TRANSIT)
-				.origin(new LatLng(originLatitude, originLongitude))
-				.destination(new LatLng(latitude, longitude)).departureTime(now).await();
-		
-		DirectionsRoute route1 = result.routes[0];
-		int numLegs = route1.legs.length;
-		DirectionsLeg lastLeg = route1.legs[numLegs - 1];
-		
-		String endAddress = lastLeg.endAddress;
-		endAddress = endAddress.replaceAll("\\d*?(.*?)[,].*", "$1");
-		
-		System.err.println(endAddress);
+	public static String getRouteToPokemon(final double longitude, final double latitude) {
+		try {
+			GeoApiContext context = new GeoApiContext().setApiKey("AIzaSyBiUikU1C1pYgqE5DWbFYqZGmn_PGktu2k");
+			DateTime now = new DateTime();
+			DirectionsResult result = DirectionsApi.newRequest(context).mode(TravelMode.TRANSIT)
+					.origin(new LatLng(originLatitude, originLongitude)).destination(new LatLng(latitude, longitude))
+					.departureTime(now).await();
 
-		String transitDepart = null;
-		String transitArrive = null;
-		
-		for (DirectionsRoute route : result.routes) {
-			for (DirectionsLeg leg : route.legs) {
-				
-				for (DirectionsStep step : leg.steps) {
-					
-					if (step.transitDetails != null) {
-						transitDepart = step.transitDetails.departureStop.name;
-						transitArrive = step.transitDetails.arrivalStop.name;
-						break;
+			String endAddress = null;
+			String transitDepart = null;
+			String transitArrive = null;
 
+			if (result.routes.length > 0) {
+				DirectionsRoute route1 = result.routes[0];
+				if (route1.legs.length > 0) {
+					int numLegs = route1.legs.length;
+					DirectionsLeg lastLeg = route1.legs[numLegs - 1];
+
+					endAddress = lastLeg.endAddress;
+					endAddress = endAddress.replaceAll("(.*?)[,].*", "$1");
+				}
+			}
+
+			
+
+			for (DirectionsRoute route : result.routes) {
+				for (DirectionsLeg leg : route.legs) {
+
+					for (DirectionsStep step : leg.steps) {
+
+						if (step.transitDetails != null) {
+							transitDepart = step.transitDetails.departureStop.name;
+							transitArrive = step.transitDetails.arrivalStop.name;
+							break;
+
+						}
 					}
 				}
 			}
-		}
-		
-		StringBuilder builder = new StringBuilder();
-		builder.append("Your nearest pokemon is at " + endAddress + ".");
-		if(transitDepart != null && transitArrive != null) {
-			builder.append(" Your journey begins at " + transitDepart + ", from where you will go to " + transitArrive );
-		}
 
-		return builder.toString();
+			StringBuilder builder = new StringBuilder();
+			builder.append("Your nearest pokemon is at " + endAddress + ".");
+			if (transitDepart != null && transitArrive != null) {
+				builder.append(
+						" Your journey begins at " + transitDepart + ", from where you will go to " + transitArrive);
+			}
+
+			return builder.toString();
+		} catch (Exception e) {
+			return "Your pokemon is shy. I can't help you find her.";
+		}
 	}
 }
