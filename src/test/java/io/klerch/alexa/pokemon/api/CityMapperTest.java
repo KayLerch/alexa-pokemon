d1d8af1a979db807ff89d7709d692a10ed75295e
nikdin <nikdin@amazon.co.uk>
2017-05-04 20:14:09 +0100

    Added assertion to unit test

--- src/test/java/io/klerch/alexa/pokemon/api/CityMapperTest.java
+++ src/test/java/io/klerch/alexa/pokemon/api/CityMapperTest.java
@@ -2,16 +2,21 @@ package io.klerch.alexa.pokemon.api;
 
 import java.io.IOException;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 import com.google.maps.errors.ApiException;
 
+
+
 public class CityMapperTest {
-	
+
 	@Test
 	public void testCityMapper() throws ApiException, InterruptedException, IOException {
 		String response = CityMapper.getRouteToPokemon(-0.128069, 51.508039);
-		System.err.println(response);
+		Assert.assertEquals(
+				"Your nearest pokemon is at 5 Trafalgar Square. Your journey begins at Piccadilly Circus Station, from where you will go to Charing Cross Underground Station",
+				response);
 	}
 
 }
