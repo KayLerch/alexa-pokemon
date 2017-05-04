1906c2c51f3ca1347908063596039254f5fede42
kaylerch <dekstar@gmx.de>
2017-05-04 19:39:38 +0200

    Initial commit

--- src/main/java/io/klerch/alexa/pokemon/handler/FindPokemonHandler.java
+++ src/main/java/io/klerch/alexa/pokemon/handler/FindPokemonHandler.java
@@ -1,18 +1,39 @@
 package io.klerch.alexa.pokemon.handler;
 
+import io.klerch.alexa.pokemon.api.CityMapper;
+import io.klerch.alexa.pokemon.api.PokemonFinder;
 import io.klerch.alexa.state.utils.AlexaStateException;
 import io.klerch.alexa.tellask.model.AlexaInput;
 import io.klerch.alexa.tellask.model.AlexaOutput;
 import io.klerch.alexa.tellask.schema.AlexaIntentHandler;
 import io.klerch.alexa.tellask.schema.annotation.AlexaIntentListener;
-import io.klerch.alexa.tellask.schema.type.AlexaIntentType;
 import io.klerch.alexa.tellask.util.AlexaRequestHandlerException;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.Optional;
 
 @AlexaIntentListener(customIntents = "FindPokemon")
 public class FindPokemonHandler implements AlexaIntentHandler {
     @Override
     public AlexaOutput handleRequest(AlexaInput input) throws AlexaRequestHandlerException, AlexaStateException {
-        return AlexaOutput.ask("wip").withReprompt(true).build();
+        final String pokemon = input.getSlotValue("Pokemon");
+
+        final Optional<Pair<Double, Double>> longlat = PokemonFinder.find(pokemon);
+
+        if (longlat.isPresent()) {
+            final String ssml = CityMapper.getRouteToPokemon(longlat.get().getLeft(), longlat.get().getRight());
+
+            if (!StringUtils.isNotBlank(ssml)) {
+                return AlexaOutput.ask("SayFoundPokemon")
+                        .putSlot("ssml", ssml)
+                        .withReprompt(true)
+                        .build();
+            }
+        }
+        return AlexaOutput.ask("SayFoundNoPokemon")
+                .putSlot("pokemon", pokemon)
+                .build();
     }
 
     @Override
